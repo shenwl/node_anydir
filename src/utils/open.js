77696f1c3c3bf18e97c430b28603e2223b428cf2
shenwl <shenweilun96@gmail.com>
2018-11-24 22:42:19 +0800

    open url fix

--- src/utils/open.js
+++ src/utils/open.js
@@ -3,9 +3,9 @@ const process = require('process');
 
 module.exports = (url) => {
   let cmd = 'open';
-  if (process.platform == 'wind32') {
+  if (process.platform === 'wind32') {
     cmd = 'start "%ProgramFiles%\Internet Explorer\iexplore.exe"';
-  } else if (process.platform == 'linux') {
+  } else if (process.platform === 'linux') {
     cmd = 'xdg-open';
   }
   childProcess.exec(`${cmd} ${url}`);
